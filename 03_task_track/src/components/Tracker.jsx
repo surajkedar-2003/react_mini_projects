56d0e866ee3226ddc0085afef239ce2d907f550b
SurajKedar-2003 <surajkedar658@gmail.com>
2024-09-19 23:04:40 +0530

    add task track application
    Implemented a to-do list with task tracking functionality.
    Added a feature to move completed tasks to a separate section.
    Introduced a task tracker with a 2/3 ratio pattern for visual tracking.
    Enabled adding tags to individual tasks for better organization.

---
+++ 03_task_track/src/components/Tracker.jsx
@@ -0,0 +1,25 @@
+
+
+function Tracker() {
+  return (
+    <>
+        
+        <section id="track_card" className="bg-cyan-500 flex flex-col w-4/12 mt-20 p-4 ml-10 max-h-96 rounded-lg">
+            <h1 className="text-3xl">Summary</h1>
+            <div className="flex flex-items center justify-center items-center text-4xl  mx-auto font-serif bg-violet-500 w-36 h-36 rounded-full">
+                <span>2/3</span>
+            </div>
+            <div>
+                Health
+            </div>
+            <div>
+                study
+            </div>
+
+        </section>
+        
+    </>
+  )
+}
+
+export default Tracker
