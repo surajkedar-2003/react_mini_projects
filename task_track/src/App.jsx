d07935d4452e1b3fb4daccb2d916e1de71f3ba42
SurajKedar-2003 <surajkedar658@gmail.com>
2024-09-19 22:51:02 +0530

    add task track application
    Implemented a to-do list with task tracking functionality.
    Added a feature to move completed tasks to a separate section.
    Introduced a task tracker with a 2/3 ratio pattern for visual tracking.
    Enabled adding tags to individual tasks for better organization.

---
+++ task_track/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header"
+import Container from "./components/Container"
+function App() {
+
+
+  return (
+    <>
+      <Header></Header>
+      <Container></Container>
+    </>
+  )
+}
+
+export default App
