6432c2ad5db4d1a9459a5bbee96a36722829ed41
SurajKedar-2003 <surajkedar658@gmail.com>
2024-09-22 22:41:29 +0530

    added animation for icon and height

--- 05_accordian/src/components/Accordian.jsx
+++ 05_accordian/src/components/Accordian.jsx
@@ -1,13 +1,15 @@
+import { useState } from "react"
 
 function Accordian() {
+    const [isActive, setisActive] = useState(false);
   return (
     <>
-        <div className="bg-white p-4 w-[500px] h-fit cursor-pointer">
+        <div className="bg-white p-4 w-[500px] h-fit cursor-pointer" onClick={()=> setisActive(!isActive)}>
             <div className="flex justify-between items-center font-semibold text-xl">
                 <h4>Lorem ipsum dolor sit amet consectetur.</h4>
-                <p className="text-3xl">+</p>
+                <p className={isActive ? 'transition-transform text-3xl rotate-45':'transition-transform text-3xl rotate-0'}>+</p>
             </div>
-            <div className={`h-0 overflow-hidden`}>
+            <div className={isActive ? 'transition-h ease-in-out duration-400 h-fit p-4' : 'transition-h ease-in-out duration-500 h-0 overflow-hidden'}>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quia, maxime laboriosam natus a facilis sit omnis assumenda, enim voluptatum dignissimos eveniet iusto sint voluptatibus quod, consequuntur repellendus itaque. Blanditiis?
             </div>
         </div>
